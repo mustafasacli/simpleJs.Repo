66127aa7723905d2bb46e7b7dc6729cda4d06a78
mustafasacli <mustasacli@hotmail.com>
2022-08-30 13:34:37 +0300

    random ops.

--- simplejs.common.1.0.0.js
+++ simplejs.common.1.0.0.js
@@ -83,3 +83,11 @@ function encodeText(value) {
 function getRandomInt(max) {
     return Math.floor(Math.random() * (max + 1));
 }
+/**
+ * 
+ */
+function getRandomArbitrary(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
